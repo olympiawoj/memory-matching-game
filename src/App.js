e91e9d7db608e1f7c46f17c45abef5ddf7521f66
olympiawoj <owojcik7@gmail.com>
2021-04-13 19:49:06 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+// image for the pokemon
+// https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png
+
+const pokemon = [
+  { id: 4, name: 'charizard' },
+  { id: 10, name: 'caterpie' },
+  { id: 77, name: 'ponyta' },
+  { id: 108, name: 'lickitung' },
+  { id: 132, name: 'ditto' },
+  { id: 133, name: 'eevee' },
+];
+
+export default function App() {
+  return <div className="app"></div>;
+}
