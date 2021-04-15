e146c40ef866fe5083bc5f2b846b06d636124747
olympiawoj <owojcik7@gmail.com>
2021-04-15 10:23:49 -0400

    Flipping Cards

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import shuffle from 'lodash.shuffle'
 
@@ -17,29 +17,55 @@ const pokemon = [
 const doublePokemon = shuffle([...pokemon, ...pokemon])
 
 export default function App() {
+  const [opened, setOpened] = useState([])
+
+  useEffect(()=>{
+    // clear cards after 2 have been selected
+    if(opened.length === 2) setTimeout(()=> setOpened([]), 800)
+
+  }, [opened])
+  
+  function flipCard(index) {
+
+    setOpened((opened) => [...opened, index])
+  }
+
   return <div className="app">
     <div className="cards">
-      {doublePokemon.map((pokemon, index) => (
-        <PokemonCard key={index} pokemon={pokemon}/>
-      ))}
+      {doublePokemon.map((pokemon, index) => {
+        let isFlipped = false;
+
+        // do some logic to check if flipped
+        if (opened.includes(index)) isFlipped = true;
+
+        return (
+          <PokemonCard
+            key={index}
+            pokemon={pokemon}
+            isFlipped={isFlipped}
+            flipCard={flipCard}
+            index={index}
+          />
+        )
+      })}
     </div>
   </div>;
 }
 
-function PokemonCard({pokemon}){
+function PokemonCard({ pokemon, isFlipped, index, flipCard}) {
   return (
-    <div className="pokemon-card flipped">
-    <div className="inner">
-      <div className="front">
-        <img
-          src={`https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png`}
-          alt={pokemon.name} width="100" />
+    <button className={`pokemon-card ${isFlipped ? 'flipped' : ''}`} onClick={()=>flipCard(index)}>
+      <div className="inner">
+        <div className="front">
+          <img
+            src={`https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png`}
+            alt={pokemon.name} width="100" />
+        </div>
+        <div className="back">
+          ?
+    </div>
       </div>
-      <div className="back">
-        ?
-    </div>
-    </div>
 
-  </div>
+    </button>
   )
 }
